e01db4a545c81bf9d4f0dbeccaf0ee81d71dbc2a
Mercy Nwoji <98951821+merbear01@users.noreply.github.com>
2024-07-10 14:29:41 +0100

    Time: 20 ms (71.08%), Space: 11.6 MB (58.33%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findMiddleIndex(self, nums):
+        psum = [0]
+        for i in range(len(nums)):
+            psum.append(psum[i] + nums[i])
+        for i in range(len(psum) - 1):
+            if psum[i] == psum[len(nums)] - psum[i + 1]:
+                return i
+        return -1
+        
