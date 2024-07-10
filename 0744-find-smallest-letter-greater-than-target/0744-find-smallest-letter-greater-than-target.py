72546556a8bd42e6b65305aba28633168660258e
Mercy Nwoji <98951821+merbear01@users.noreply.github.com>
2024-07-10 18:11:36 +0100

    Time: 72 ms (37.13%), Space: 13.6 MB (48.66%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        for char in letters:
+            if char > target:
+                return char
+            
+        return letters[0]
+            
+            
+        
