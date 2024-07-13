e5f2823c2b4186d18c18b30bb9cb0addf693716f
Mercy Nwoji <98951821+merbear01@users.noreply.github.com>
2024-07-13 21:59:44 +0100

    Time: 76 ms (94.42%), Space: 12.7 MB (82.05%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
@@ -4,12 +4,18 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        new_lst = []
-        for l in grid:
-            new_lst.extend(l)
-        # return lst
         negative_pot = 0
-        for items in new_lst:
-            if items < 0:
-                negative_pot += 1
+        for rows in grid:
+            for items in rows:
+                if items < 0:
+                    negative_pot += 1
         return negative_pot
+        # new_lst = []
+        # for l in grid:
+        #     new_lst.extend(l)
+        # # return lst
+        # negative_pot = 0
+        # for items in new_lst:
+        #     if items < 0:
+        #         negative_pot += 1
+        # return negative_pot
