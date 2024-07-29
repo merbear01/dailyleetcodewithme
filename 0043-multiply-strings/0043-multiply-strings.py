fc6bffe51cd8984d1292628352c50ef2a3651dd5
Mercy Nwoji <98951821+merbear01@users.noreply.github.com>
2024-07-29 11:35:08 +0100

    Time: 10 ms (92.76%), Space: 11.7 MB (22.51%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num1 = int(num1)
+        num2 = int(num2)
+        mult = num1 * num2
+        return str(mult)
